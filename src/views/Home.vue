ae1f99bd7bba7695bce745a0a5083f9e6c23b2b8
Arthur Iques <98358475+codebyarths@users.noreply.github.com>
2023-03-27 17:29:45 -0400

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <Banner/>
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    
+    <burgers-form/>
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue';
+import BurgersForm from '@/components/BurgersForm.vue';
+
+
+
+export default{
+  name: "Home",
+  components:{
+    Banner,
+    BurgersForm
+  }
+}
+
+</script>
